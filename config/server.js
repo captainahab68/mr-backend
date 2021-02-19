fb4e08420d397a9e11f9c764c3e618bc26fea089
Alex Miller <ajm9594@gmail.com>
2021-02-19 09:39:43 -0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '56c65315db1ec0a62c49a43b78c779ec'),
+    },
+  },
+});
